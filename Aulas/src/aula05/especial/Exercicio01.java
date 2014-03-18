55729d05bcf6f253f61a76ea470a8a1716072a57
Fabricio Bizotto <fabricio.bizotto@gmail.com>
2014-03-18 20:04:13 -0300

    commit aula especial

---
+++ Aulas/src/aula05/especial/Exercicio01.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula05.especial;
+
+/**
+ * fazer um programa utilizando a estrutura de controle if para verificar se um
+ * carro tem combustível. Se sim escrever que o carro vai andar senão escrever
+ * que o carro precisa de combustível.
+ *
+ * @author fabricio
+ */
+public class Exercicio01 {
+
+    public static void main(String[] args) {
+
+        boolean possuiCombustivel = false;
+
+        if (possuiCombustivel == true) {
+            System.out.println("O carro vai andar.");
+        } else {
+            System.out.println("O carro precisa de combustível.");
+        }
+
+    }
+}
---
+++ Aulas/src/aula05/especial/Exercicio02.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula05.especial;
+
+/**
+ * Escrever um programa que verifique dois números e diga qual é o maior.
+ *
+ * @author fabricio
+ */
+public class Exercicio02 {
+
+    public static void main(String[] args) {
+        int n1 = 10;
+        int n2 = 20;
+
+        if (n1 > n2) {
+            System.out.println(n1);
+        } else {
+            System.out.println(n2);
+        }
+    }
+}
